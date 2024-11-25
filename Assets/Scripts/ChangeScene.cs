d224341b8be00b9e7d20aec82d45b19b17245796
ThtWasBrutal <eunruh73@gmail.com>
2024-11-25 10:00:18 -0800

    lighting
    Nick is a bitch

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(2);
+        }
+
+    }
+
+}
