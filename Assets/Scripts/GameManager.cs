436e5317274c7e1a6ec44cdde886e42623af1d8a
purplekid1 <71616134+purplekid1@users.noreply.github.com>
2024-12-05 10:25:27 -0800

    animation for monster start

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,29 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using TMPro;
 
-public class GameManger : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public bool isPaused = false;
 
     public GameObject pauseMenu;
     public PlayerController playerData;
 
-    public Image healthBar;
-  
     // Start is called before the first frame update
     void Start()
     {
-        if (SceneManager.GetActiveScene().name != "Main Menu")
-            playerData = GameObject.Find("Player").transform.GetChild(0).GetComponent<PlayerController>();
+        playerData = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerData != null)
+        {
+            
+            if (!isPaused && Input.GetKeyDown(KeyCode.Escape))
+            {
+                isPaused = true;
+
+                pauseMenu.SetActive(true);
+
+                Time.timeScale = 0;
+
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+
+            else if (isPaused && Input.GetKeyDown(KeyCode.Escape))
+                Resume();
+        }
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+
+        pauseMenu.SetActive(false);
+
+        Time.timeScale = 1;
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartLevel()
+    {
+        LoadLevel(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadLevel(int sceneID)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(sceneID);
     }
 }
