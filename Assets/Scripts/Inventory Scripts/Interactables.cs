595bf95932e8548851f3fe2a6a739f1179317993
EpicGamer5888 <63515818+EpicGamer5888@users.noreply.github.com>
2024-12-15 23:44:14 -0800

    Inventory

---
+++ Assets/Scripts/Inventory Scripts/Interactables.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+public class Interactables : MonoBehaviour
+{
+    public float radius = 3f;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
