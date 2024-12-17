02fc0f7f21509b36e8d6ae95c079aba70cd0c4d8
EpicGamer5888 <63515818+EpicGamer5888@users.noreply.github.com>
2024-12-17 11:38:42 -0800

    Fixed Scene

--- Assets/Scripts/Enemy Scripts/EnemyAnimationController.cs
+++ Assets/Scripts/Enemy Scripts/EnemyAnimationController.cs
@@ -1,9 +1,8 @@
 using UnityEngine;
 using UnityEngine.AI;
-<<<<<<< HEAD
-=======
+
 using UnityEngine.InputSystem;
->>>>>>> e02c994376d1dab36ba9ed513b621011ce15a006
+
 
 public class EnemyAnimationController : MonoBehaviour
 {
@@ -23,18 +22,18 @@ public class EnemyAnimationController : MonoBehaviour
         
 
         // Check the velocity of the NavMeshAgent
-<<<<<<< HEAD
+
         if (agent.acceleration >= 0.01f && agent.acceleration !>= 10f && agent.remainingDistance > agent.stoppingDistance) // Moving
-=======
+
         if (RM.Running) // Running
->>>>>>> e0aebd014b8bfbc935b46bc5d205efdaf225d79f
+
         {
             animator.SetBool("IsWalking", true);
-<<<<<<< HEAD
+
             animator.SetBool("IsRunning", true);
-=======
+
             //animator.SetBool("IsRunning", false);
->>>>>>> e02c994376d1dab36ba9ed513b621011ce15a006
+
         }
         else if (RM.Walking)// Walking
         {
