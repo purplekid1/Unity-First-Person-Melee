cff4384891447c9c838a582e9217409202426262
EpicGamer5888 <63515818+EpicGamer5888@users.noreply.github.com>
2024-12-20 06:26:43 -0800

    FarmHouse scene done

--- Assets/Scripts/Game Scripts/ChangeScene.cs
+++ Assets/Scripts/Game Scripts/ChangeScene.cs
@@ -9,7 +9,7 @@ public class ChangeScene : MonoBehaviour
     {
         if (Other.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(2);
         }
 
     }
