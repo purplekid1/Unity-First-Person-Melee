efd0d224830a05221feacacb21a4823fd19d2005
EpicGamer5888 <63515818+EpicGamer5888@users.noreply.github.com>
2024-12-20 06:44:35 -0800

    both house scenes done models do need to be made not able to go through

---
+++ Assets/Scripts/Game Scripts/ChangeScene3.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene3 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider Other)
+    {
+        if (Other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(2);
+        }
+
+    }
+
+}
